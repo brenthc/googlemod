23ce0debbe9de7f1a63cbf74c28dfbd6a502c4cf
Brent Woodruff <brent@hashicorp.com>
2023-07-19 16:54:43 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  version = "2.14.0"
+}
+provider "google-beta" {
+  version = "2.14.0"
+}
+provider "null" {
+  version = "2.1.2"
+}
+provider "random" {
+  version = "2.2.1"
+}
+
+module "project-factory" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "2.4.1"
+
+  billing_account     = "ABCDEF-ABCDEF-ABCDEF"
+  name                = "pf-test-1"
+  org_id              = "1234567890"
+}
